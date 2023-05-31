6fa507bc15c03e93d133f98fa8fe0fea33050732
JESUS_ARNOLDO_JURADO_GARCIA <a.20308051281011@cbtis128.edu.mx>
2023-05-31 15:26:36 +0000

    formulario validator

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,372 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(TabBarDemo());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class TabBarDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+        debugShowCheckedModeBanner: false,
+        home: DefaultTabController(
+          length: 4,
+          child: Scaffold(
+              appBar: AppBar(
+                bottom: const TabBar(
+                  tabs: [
+                    Tab(icon: Icon(Icons.add_reaction)),
+                    Tab(icon: Icon(Icons.add_home)),
+                    Tab(icon: Icon(Icons.add_box)),
+                    Tab(icon: Icon(Icons.directions_bike)),
+                  ],
+                ),
+                title: const Text('Formularios act4'),
+              ),
+              body: TabBarView(
+                children: const [
+                  MyCustomForm(),
+                  MyCustomForm2(),
+                  MyCustomForm3(),
+                  MyCustomForm4(),
+                ],
+              )),
+        ));
+  }
+}
+
+// Create a Form widget.
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+class MyCustomFormState extends State<MyCustomForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Ingresa id'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'ingresa tu nombre'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'ingresa tu apellido'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'ingresa tu correo'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'fecha nacimiento'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Procesando datos')),
+                  );
+                }
+              },
+              child: const Text('Enter'),
+            ),
+          ),
+        ],
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MyCustomForm2 extends StatefulWidget {
+  const MyCustomForm2({super.key});
+
+  @override
+  MyCustomFormState2 createState() {
+    return MyCustomFormState2();
+  }
+}
+
+class MyCustomFormState2 extends State<MyCustomForm2> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Ingresa id'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'ingresa tu nombre'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'ingresa tu apellido'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'ingresa tu departamento'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'correo'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Procesando datos')),
+                  );
+                }
+              },
+              child: const Text('Enter'),
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+    );
+  }
+}
+
+class MyCustomForm3 extends StatefulWidget {
+  const MyCustomForm3({super.key});
+
+  @override
+  MyCustomFormState3 createState() {
+    return MyCustomFormState3();
+  }
+}
+
+class MyCustomFormState3 extends State<MyCustomForm3> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Ingresa id'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'ingresa tu nombre'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'ingresa tu apellido'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'id produtos preferidos'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'edad'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Procesando datos')),
+                  );
+                }
+              },
+              child: const Text('Enter'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyCustomForm4 extends StatefulWidget {
+  const MyCustomForm4({super.key});
+
+  @override
+  MyCustomFormState4 createState() {
+    return MyCustomFormState4();
+  }
+}
+
+class MyCustomFormState4 extends State<MyCustomForm4> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Ingresa id producto'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'ingresa descripcion'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'ingresa cantidad'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'presio'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'id provedor'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'porfavor ingrese un dato';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Procesando datos')),
+                  );
+                }
+              },
+              child: const Text('Enter'),
+            ),
+          ),
+        ],
       ),
     );
   }
